a07e5a22c29d306c228317ebaf8959631f7e7efb
DIOUF-MOR <daaroukhoudos04@gmail.com>
2022-03-17 19:06:43 +0000

    TP1 T1:HTML, T2:CSS ET js

---
+++ TP1-NOTIFICATIONS/scripte.js
@@ -0,0 +1,17 @@
+const button = document.querySelectorAll('button');
+
+const span = document.querySelector('.span2');
+
+    for (let i = 0; i < button.length; i++) {
+        const btn = button[i];
+        btn.addEventListener('click',function(){
+           const div = document.createElement('div')
+           div.innerText = 'Mon Projet 4'
+           div.classList.add(span.className , btn.className)
+           span.appendChild(div);
+        })
+        setTimeout(() => {
+            div.remove();
+        }, 1);
+    }
+
